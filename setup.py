9eb184a674f9a03628915e6f5eb042fa3c965326
Ranit Sarkar <121813854+ranitsarkar-DS@users.noreply.github.com>
2023-10-15 22:51:51 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Ranit Sarkar',
+    author_email= 'ranitsarkar210902@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
